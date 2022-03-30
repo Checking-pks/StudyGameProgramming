76d1c11e937733e0e8cff1ff4c69e51f298535ac
Checking <qkrrudtn23@naver.com>
2022-03-30 17:06:34 +0900

    Week 05
    Create Target,
    Hit Target System

---
+++ StudyGameProgramming/Assets/02.Scripts/Bullet.cs
@@ -0,0 +1,26 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Bullet : MonoBehaviour
+{
+    public float hp;
+    
+    void Start()
+    {
+        hp = 10;
+        Destroy(gameObject, hp);
+    }
+
+    void Update()
+    {
+        
+    }
+
+    void OnTriggerEnter(Collider other)
+    {
+        if (other.tag == "Wall")
+            Destroy(other.gameObject);
+        Destroy(gameObject);
+    }
+}
