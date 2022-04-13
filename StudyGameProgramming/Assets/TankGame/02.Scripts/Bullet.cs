bf380c9d3d5e0aca9bd467ca6def0662531ee9d1
Checking <qkrrudtn23@naver.com>
2022-04-13 18:04:36 +0900

    Week07

---
+++ StudyGameProgramming/Assets/TankGame/02.Scripts/Bullet.cs
@@ -0,0 +1,15 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Bullet : MonoBehaviour
+{
+    public float health;
+    public float damage;
+    public string firedObject;
+    
+    void Start()
+    {
+        Destroy(gameObject, health);
+    }
+}
---
+++ StudyGameProgramming/Assets/TankGame/02.Scripts/Camera.cs
@@ -0,0 +1,20 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Camera : MonoBehaviour
+{
+    #region 변수 설정
+    public GameObject player;
+    public Vector3 cameraPosition;
+    #endregion
+
+    // Update is called once per frame
+    void Update()
+    {
+        if (player != null)
+        {
+            transform.position = player.transform.position + cameraPosition;
+        }
+    }
+}
